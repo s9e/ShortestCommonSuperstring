7f9ebd77781e1a88c6d2ad3acb2d9fea73aed416
JoshyPHP <s9e.dev@gmail.com>
2016-12-16 18:55:13 +0100

    Initial commit

---
+++ src/ShortestCommonSuperstring.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+* @package   s9e\ShortestCommonSuperstring
+* @copyright Copyright (c) 2016 The s9e Authors
+* @license   http://www.opensource.org/licenses/mit-license.php The MIT License
+*/
+namespace s9e\ShortestCommonSuperstring;
+
+class ShortestCommonSuperstring
+{
+	/**
+	* Get the shortest string that contains all given strings
+	*
+	* @param  string[] $strings
+	* @return string
+	*/
+	public function getShortest(array $strings)
+	{
+	}
+}
